b4110c075014568947ec9539d1a4ab1ee457c32f
vantinfy@gmail.com <vantinfy@gmail.com>
2022-05-27 00:49:05 +0800

    限定池增加大保底

--- module/mod_pool.go
+++ module/mod_pool.go
@@ -13,6 +13,7 @@ type PoolInfo struct {
 	PoolId        int
 	FiveDrawTimes int // 该池子抽取了多少次还未出5星，出货后重置
 	FourDrawTimes int // 该池子抽取了多少次还未出4星，出货后重置
+	IsUpRole      int // 保底五星是否为限定角色
 }
 
 func (p *ModPool) UpPoolDraw(times int) {
@@ -67,6 +68,8 @@ func (p *ModPool) UpPoolDraw(times int) {
 					p.UpPoolInfo.FiveDrawTimes = 0
 					p.UpPoolInfo.FourDrawTimes++
 					cnt5++
+					// up池处理大小保底问题
+					card = p.handle5star(card)
 				} else if role.Star == 4 { // 出货4星角色后重置次数
 					p.UpPoolInfo.FourDrawTimes = 0
 					p.UpPoolInfo.FiveDrawTimes++
@@ -77,7 +80,7 @@ func (p *ModPool) UpPoolDraw(times int) {
 				if weapon == nil {
 					fmt.Println("抽卡结果掉落：武器配置信息错误")
 					return
-				} else if weapon.Star == 5 { // 五星武器
+				} else if weapon.Star == 5 { // 五星武器情况 限定池是不会走到这里来的，保底五星是角色（应该）
 					p.UpPoolInfo.FiveDrawTimes = 0
 					p.UpPoolInfo.FourDrawTimes++
 					cnt5++
@@ -98,4 +101,27 @@ func (p *ModPool) UpPoolDraw(times int) {
 		fmt.Printf("%s\t次数:%d\n", csvs.GetItemName(k), v)
 	}
 	fmt.Printf("累计5星:%d，累计四星:%d\n", cnt5, cnt4)
+}
+
+// todo 对应用户抽限定池1/2，这里map也要对应改变
+func (p *ModPool) handle5star(card *csvs.ConfigDrop) *csvs.ConfigDrop {
+	// 如果上次5星不是限定，则本次5星结果强制替换为限定池中的角色
+	if p.UpPoolInfo.IsUpRole == csvs.LOGIC_TRUE {
+		dropGroup := csvs.ConfigDropMap[100012]
+		if dropGroup == nil {
+			fmt.Println("大保底抽卡配置数据异常")
+			return card
+		}
+		card = csvs.GetRandDropRecursion(dropGroup)
+		if card == nil {
+			fmt.Println("大保底抽卡结果数据异常")
+			return card
+		}
+	}
+	if card.DropId == 100012 { // 结果是5星限定角色，重置大保底
+		p.UpPoolInfo.IsUpRole = csvs.LOGIC_FALSE
+	} else { // 歪了...
+		p.UpPoolInfo.IsUpRole = csvs.LOGIC_TRUE
+	}
+	return card
 }
